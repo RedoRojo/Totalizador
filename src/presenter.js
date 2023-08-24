58357fd995e87afe1178d8b85a3fa85399c2cda3
RedoRojo <cristhiabnvictorrm@gmail.com>
2023-08-24 09:39:45 -0400

    Ingresar estado y mostrarlo implementado

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,18 @@
 import sumar from "./sumador";
 
 const cantItems = document.querySelector("#cant-items");
- const precioItem = document.querySelector("#precio");
+const precioItem = document.querySelector("#precio");
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
-
+const estado = document.querySelector("#estado")
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cant = Number.parseInt(cantItems.value);
   const precio=Number.parseInt(precioItem.value);
+  const estadoTexto = estado.value; 
   div.innerHTML = "<p>" + cant + "</p>";
   div.innerHTML += "<p>" + precio + "</p>";
+  div.innerHTML += "<p>" + estadoTexto + "</p>";
+  
 });
