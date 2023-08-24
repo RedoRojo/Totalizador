e681120c97ba7437bee96aeb6b2578e75d6ff112
RedoRojo <cristhiabnvictorrm@gmail.com>
2023-08-24 09:24:47 -0400

    Mostrar cantidad ingresada implementada

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,14 @@
 import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const cantItems = document.querySelector("#cant-items");
+// const second = document.querySelector("#segundo-numero");
+const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const cant = Number.parseInt(cantItems.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + cant + "</p>";
 });
